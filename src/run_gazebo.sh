76e3f68cec12737dec0ef1202ab920ef0e720130
MohamedAfifii <eng_muhammad.a@hotmail.com>
2018-06-26 02:37:04 +0200

    Branch

--- src/run_gazebo.sh
+++ src/run_gazebo.sh
@@ -6,4 +6,4 @@ sudo killall rviz
 sudo killall roscore
 sudo killall rosmaster
 
-roslaunch mybot_gazebo mybot_world.launch simulation:=true
+roslaunch mybot_gazebo mybot_world.launch simulation:=false
